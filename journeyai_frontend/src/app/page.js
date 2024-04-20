4c6a0b99e1a252b34cdbfc320fb937f9053eeb84
bpnguyen107 <bpnguyen107@g.ucla.edu>
2024-04-19 21:51:37 -0700

    image upload

--- journeyai_frontend/src/app/page.js
+++ journeyai_frontend/src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import ImageUpload from "./upload/page";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
+        <ImageUpload />
         <p>
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.js</code>
