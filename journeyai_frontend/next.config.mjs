ca39cc85657145b70a62877f132ee52e210c76e0
Tommy Nguyen <tommyhnguyen03@gmail.com>
2024-04-20 01:38:42 -0700

    feat: set up scss

--- journeyai_frontend/next.config.mjs
+++ journeyai_frontend/next.config.mjs
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from 'path';
+
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const sassOptions = {
+  includePaths: [path.join(__dirname, 'styles')],
+};
+
+const nextConfig = {
+  sassOptions,
+};
 
 export default nextConfig;
